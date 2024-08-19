bd1b58a29114fb0cc9b5d508c24aeaf744ad97fe
tuntun <760523867@qq.com>
2024-08-19 23:34:02 +0800

    feat: code

--- src/content.tsx
+++ src/content.tsx
@@ -1,7 +1,7 @@
 import cssText from "data-text:~style.css"
 import type { PlasmoCSConfig } from "plasmo"
 
-import { CountButton } from "~features/count-button"
+import { Button } from "~components/ui/button"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://www.plasmo.com/*"]
@@ -16,7 +16,7 @@ export const getStyle = () => {
 const PlasmoOverlay = () => {
   return (
     <div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8">
-      <CountButton />
+      <Button>test</Button>
     </div>
   )
 }
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,11 +1,11 @@
-import { CountButton } from "~features/count-button"
+import { Button } from "~components/ui/button"
 
 import "~style.css"
 
 function IndexPopup() {
   return (
     <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <CountButton />
+      <Button>test</Button>
     </div>
   )
 }
